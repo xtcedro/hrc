aa873a6d490e443e1fb2d828f7003fcd291946b9
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-02-18 11:38:07 -0600

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,6 @@
 // Import only setupNavigation from navigation.js
 import { setupNavigation } from './navigation.js';
+import { loadHeader, loadFooter } from './load-components.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     loadHeader();
